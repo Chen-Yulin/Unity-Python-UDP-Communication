af8aee10deb10e0f9bcb923d67aff9f738b049bd
chenyulin <chenyulin@sjtu.edu.cn>
2024-06-13 19:52:25 +0800

    feat. change UDP Msg

--- server.py
+++ server.py
@@ -14,15 +14,40 @@ import time
 # Create UDP socket to use for sending (and receiving)
 sock = U.UdpComms(udpIP="127.0.0.1", portTX=8000, portRX=8001, enableRX=True, suppressWarnings=True)
 
-i = 0
+cat = "cat MIAO~"
+pos = [0,0,0]
+rot = [0,0,0]
+size = [0.2,0.2,0.2]
+
+direction = -1
+
+def ObjectMsg(cat,px,py,pz,rx,ry,rz,sx,sy,sz):
+    res = "{Object Detection}\n"
+    res += cat + "\n"
+    res += str(px) + "\n"
+    res += str(py) + "\n"
+    res += str(pz) + "\n"
+    res += str(rx) + "\n"
+    res += str(ry) + "\n"
+    res += str(rz) + "\n"
+    res += str(sx) + "\n"
+    res += str(sy) + "\n"
+    res += str(sz)
+    return res
 
 while True:
-    sock.SendData('Sent from Python: ' + str(i)) # Send this string to other application
-    i += 1
+    sock.SendData(ObjectMsg(cat, pos[0], pos[1], pos[2], rot[0], rot[1], rot[2], size[0], size[1], size[2])) # Send this string to other application
+    rot[1] += 1
+    pos[0] += 0.02*direction
+
+    if pos[0] > 1:
+        direction = -1
+    elif pos[0] < -1:
+        direction = 1
 
     data = sock.ReadReceivedData() # read data
 
     if data != None: # if NEW data has been received since last ReadReceivedData function call
         print(data) # print new received data
 
-    time.sleep(1)
+    time.sleep(0.05)
